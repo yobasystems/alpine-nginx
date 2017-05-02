0da51922cd88931b3f0e78c3bd530110c17df09b
Dominic Taylor <dominic@dominictaylor.co.uk>
2017-05-02 10:33:08 +0100

    add ssh auth for git service for private repos

---
+++ alpine-nginx-git-ssh/files/run.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+#add ssh private key for auth to git service of choice
+mkdir -p ~/.ssh
+echo "${SSH_PRIVATE_KEY}" | tr -d '\r' > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan -H '${REPO_URL}' >> ~/.ssh/known_hosts
+#clone repo with passwordless auth
+git clone --depth=1 --branch=master ${REPO} /etc/nginx/html
+rm -rf /etc/nginx/html/.git
+#start nginx
+nginx
